51f953996c15a3d61892477b35ce433bab9efb08
David J Shuckerow <djshuckerow@hotmail.com>
2014-11-19 23:42:07 -0600

    added mediator, changed file names to lowercase.

---
+++ PebbleGame/src/mediator.py
@@ -0,0 +1,20 @@
+'''
+Mediator.py
+by: David Shuckerow (djs0017@auburn.edu) and Carlos Lemus (cal0018@auburn.edu)
+date: 11/19/2014
+
+The Mediator class implements the mediator design pattern.
+It is responsible for coordinating the Board model, the View of the board, and 
+the AI Controller(s) responsible for instructing how to change the board state.
+'''
+__author__ = "Carlos Lemus, David Shuckerow"
+__license__ = "MIT"
+
+from board import Board
+
+class Mediator:
+	"""
+	Stub for now.
+	"""
+	def __init__(self):
+		board = Board()
